a0f280945f1b81b88fe93d6965c89c619b44f4ad
ArturoQuevedo <daquevedoa@unal.edu.co>
2020-04-12 14:36:31 -0500

    Update generator.c

--- generator.c
+++ generator.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include<time.h>
 
-struct Pet
+struct dogType
 {
     char name[32];
     char type[32];
@@ -11,86 +12,47 @@ struct Pet
     int height;
     float weight;
     char gender;
-    long next;
+    int prev;
 };
+int structSize = sizeof(struct dogType);
 
-int write(void *ap)
+int readSomeDogs() //Lee uno de cada 1M perros
 {
-    struct Pet *data;
-    data = ap;
+    int i = 0;
 
     FILE *outfile;
-    if ((outfile = fopen("datadogs.dat", "a+")) == NULL)
+    outfile = fopen("datadogs.dat", "rb");
+    if (!outfile)
     {
-        printf("Error opening file in write\n");
-        return 1;
+        fprintf(stderr, "Error opening file 'nombresMascotas.txt'");
+        return EXIT_FAILURE;
     }
-    fwrite(data, sizeof(struct Pet), 1, outfile);
+
+    struct dogType *pet;
+    pet = malloc(structSize);
+
+    while (fread(pet, structSize, 1, outfile))
+    {
+        if (i % 2000000 == 0)
+        {
+            printf("\n %i", i);
+            printf("\nNombre: %s", pet->name);
+            printf("\nTipo: %s", pet->type);
+            printf("\tEdad: %i", pet->age);
+            printf("\nRaza: %s", pet->race);
+            printf("\nAltura: %i", pet->height);
+            printf("\tPeso: %0.2f", pet->weight);
+            printf("\nSexo: %c", pet->gender);
+            printf("\nPrev: %i \n", pet->prev);            
+
+            
+        }
+        i++;
+    }
+    free(pet);
     fclose(outfile);
-}
-
-int read()
-{
-    FILE *infile;
-    if ((infile = fopen("datadogs.dat", "rb")) == NULL)
-    {
-        printf("Error opening file in read\n");
-        return 1;
-    }
-
-    rewind(infile);
-    struct Pet pet;
-
-    while (fread(&pet, sizeof(struct Pet), 1, infile))
-    {
-        printf("Nombre: %s", pet.name);
-        printf("\nTipo: %s", pet.type);
-        printf("\nEdad: %i", pet.age);
-        printf("\nRaza: %s", pet.race);
-        printf("\nAltura: %i", pet.height);
-        printf("\nPeso: %0.2f", pet.weight);
-        printf("\nSexo: %c", pet.gender);
-        printf("\nNext: %li \n\n", pet.next);
-    }
-    fclose(infile);
-
     return 0;
-}
 
-int change(void *ap, long pos)
-{
-    struct Pet *data;
-    data = ap;
-
-    FILE *outfile;
-    if ((outfile = fopen("datadogs.dat", "r+")) == NULL)
-    {
-        printf("Error opening file in change\n");
-        return 1;
-    }
-    fseek(outfile, sizeof(struct Pet) * pos, SEEK_SET);
-    fwrite(data, sizeof(struct Pet), 1, outfile);
-    fclose(outfile);
-    //printf("\n write: %li \n", pos);
-    return 0;
-}
-
-int changenextNode(long value, long pos)
-{
-    struct Pet pet;
-
-    FILE *infile;
-    if ((infile = fopen("datadogs.dat", "r+")) == NULL)
-    {
-        printf("Error opening file in change next node\n");
-        return 1;
-    }
-    fseek(infile, sizeof(struct Pet) * pos, SEEK_SET);
-    fread(&pet, sizeof(struct Pet), 1, infile);
-    pet.next = value;
-    fclose(infile);
-    change(&pet, pos);
-    return 0;
 }
 
 char *setRace()
@@ -150,223 +112,91 @@ char *setRace()
     default:
         break;
     }
-
-    free(race);
-    return (race);
 }
 
-long longpow(long x, int n)
-{
-    long i;
-    long number = 1;
-
-    for (int i = 0; i < n; i++)
-    {
-        number *= x;
-    }
-    
-    return number;
-}
-
-//actualmente sin uso
-int readpos(long pos)
-{
-
-    FILE *file;
-    file = fopen("datadogs.dat", "r+");
-
-    fseek(file, sizeof(struct Pet) * pos, SEEK_SET);
-    struct Pet pet;
-    fread(&pet, sizeof(struct Pet), 1, file);
-    printf("\n/////// readpos ///////\n");
-    printf("Nombre: %s", pet.name);
-    printf("\nTipo: %s", pet.type);
-    printf("\nEdad: %i", pet.age);
-    printf("\nRaza: %s", pet.race);
-    printf("\nAltura: %i", pet.height);
-    printf("\nPeso: %0.2f", pet.weight);
-    printf("\nSexo: %c", pet.gender);
-    printf("\nNext: %li \n", pet.next);
-    fclose(file);
-    return 0;
-}
-
-long getnextNode(long pos)
-{
-    FILE *file;
-    file = fopen("datadogs.dat", "r+");
-
-    fseek(file, sizeof(struct Pet) * pos, SEEK_SET);
-    struct Pet pet;
-
-    fread(&pet, sizeof(struct Pet), 1, file);
-    //printf("\nnext node: %li \n", pet.next);
-    fclose(file);
-    return pet.next;
-}
-
-//----funciones para la tabla HASH----
-
-//calcula el resultado de la funcion hash y lo retorna
-long hashfunction(char name[32])
-{
-    long result = 0;
-    long i = 0;
-    while (name[i] != '\0')
-    {
-        if ((long)name[i] != 10)
-        {
-            result = result + (((long)name[i])*longpow(167,i));
-        }        
-        i++;
-    }
-    result = result % 1801;
-    //printf("\nhash result: %li \n", result);
-    
-    return result;
-}
-
-//devuelve el id almacenado en la posicion dada
-long readhash(long pos)
-{
-    FILE *file;
-    file = fopen("hash.bin", "r+");
-
-    fseek(file, sizeof(long) * pos, SEEK_SET);
-    long id;
-    fread(&id, sizeof(long), 1, file);
-    //printf("\n/////// id: ///////\n");
-    //printf("pos: %li", pos);
-    //printf("\nid: %li \n", id);
-    fclose(file);
-    return id;
-}
-
-//cambia el valor almacenado en una posicion dada
-int writehash(int pos, long value)
-{
-    FILE *ftc;
-    ftc = fopen("hash.bin", "r+");
-    fseek(ftc, sizeof(long) * pos, SEEK_SET);
-    fwrite(&value, sizeof(long), 1, ftc);
-    fclose(ftc);
-    //printf("\nwritehash de value: %li \n", value);
-    return 0;
-}
-
-//------------------------------MAIN----------------------------------//
 int main(int argc, char *argv[])
 {
+
+    //Para los numeros aleatorios
+    time_t t;
+    srand((unsigned)time(&t));
+
+    //Variables para leer los nombres
     char *line_buf = NULL;
     size_t line_buf_size = 0;
-    long line_count = 0;
+    int line_count = 0;
     ssize_t line_size;
-    long temp = -1;
-    long current_node = -1;
 
-    FILE *fp = fopen("nombresMascotas.txt", "r+");
-    if (!fp)
+    FILE *namesfile = fopen("nombresMascotas.txt", "r+");
+    if (!namesfile)
     {
-        fprintf(stderr, "Error opening file 'names.txt'");
+        fprintf(stderr, "Error opening file 'nombresMascotas.txt'");
         return EXIT_FAILURE;
     }
-    line_size = getline(&line_buf, &line_buf_size, fp);
+    line_size = getline(&line_buf, &line_buf_size, namesfile);
 
-    //GENERADOR DE TABLA HASH EN HASH.BIN
-    FILE *file;
-    file = fopen("hash.bin", "w+");
-
-    for (int i = 0; i < 1801; i++)
+    //Crear datadogs
+    FILE *dogsfile = fopen("datadogs.dat", "w+");
+    if (!dogsfile)
     {
-        fwrite(&temp, sizeof(long), 1, file);
+        fprintf(stderr, "Error opening file 'datadogs.dat'");
+        return EXIT_FAILURE;
     }
-    fclose(file);
 
-    //TRUNCATE datadogs.dat
-    file = fopen("datadogs.dat", "w+");
-    fclose(file);
-    struct Pet pet;
+    struct dogType *pet;
+    pet = malloc(structSize);
 
     //GENERADOR RANDOM DE MASCOTAS EN DATADOGS.DAT
-    for (long i = 0; i < 10000000; i++)
+    for (int i = 0; i < 8; i++)
     {
         line_count++;
-        strcpy(pet.name, line_buf);
-        
-        //sprintf(pet.name, "ABCD"); //Dato quemado para pruebasssssssss
-        line_size = getline(&line_buf, &line_buf_size, fp);
+
+        strcpy(pet->name, line_buf);
+        //AGREGAR UN TOLOWER CASE!!!!
+
+        if(i>=3 && i<6){
+            strcpy(pet->name, "Abanero\n");
+        }
+        if(i==7){
+            strcpy(pet->name, "Aarón\n");
+        }
+        line_size = getline(&line_buf, &line_buf_size, namesfile);
+
+        //Si se acaban los nombres, vuelva a leer nombresMascotas.txt
         if (line_count >= 1716)
         {
-            rewind(fp);
+            rewind(namesfile);
             line_count = 0;
         }
 
-        //EL NOMBRE SE LEE DEL TXT DE NOMBRES
-        sprintf(pet.type, "Perro");
-
-        pet.age = rand() % 14;
-        pet.height = rand() % 110;
-        pet.weight = (rand() % 101) + ((rand() % 100) / 100.0);
-
-        strcpy(pet.race, setRace());
+        sprintf(pet->type, "Perro");
+        pet->age = rand() % 14;
+        pet->height = (rand() % 110) + 1;
+        pet->weight = ((rand() % 100) + 1) + ((rand() % 100) / 100.0);
+        strcpy(pet->race, setRace());
 
         switch (rand() % 2)
         {
         case 0:
-            pet.gender = 'F';
+            pet->gender = 'H';
             break;
         case 1:
-            pet.gender = 'M';
+            pet->gender = 'M';
             break;
         default:
             break;
         }
-        printf("\n %li \n",i);
-        printf("Nombre: %s", pet.name);
-        //printf("\t %s", pet.type);
-        //printf("\t%i años", pet.age);
-        //printf("\n%s", pet.race);
-        //printf("\t%i cm", pet.height);
-        //printf("\t %0.2f kg", pet.weight);
-        //printf("\nSexo: %c \n", pet.gender);
+        pet->prev=-1;
 
-        //consulta con funcion hash en busca de estructuras con el mismo resultado
-        temp = hashfunction(pet.name);
-        current_node = readhash(temp);
-
-        //si es -1 el valor hash no tiene estructura asignada
-        if (current_node == -1)
-        {
-            //pet actual se convierte en head y tail de la lista
-            pet.next = -1;
-
-        }
-        else
-        {
-            pet.next = current_node;
-            //while (1 == 1)
-            //{
-            //    //temp toma el valor del next node que se tiene guardado
-            //    temp = getnextNode(current_node);
-            //    if (temp == -1)
-            //    {
-            //        pet.prev = current_node;
-            //        pet.next = -1;
-            //        changenextNode(i, current_node);
-            //        break;
-            //    }
-            //    current_node = temp;
-            //}
-        }
-
-        //se actualiza la hash table con pet como cabeza del nodo
-        writehash(temp, i);
-        
-        //se escribe la estructura en el archivo .dat
-        write(&pet);
+        fwrite(pet, structSize, 1, dogsfile);
     }
-    fclose(fp);
-    readpos(1716);
+    free(pet);
+    fclose(namesfile);
+    fclose(dogsfile);
+
+    //readSomeDogs();
+
+
 
     return 0;
 }
